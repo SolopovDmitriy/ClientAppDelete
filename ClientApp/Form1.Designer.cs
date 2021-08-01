cef03aa89fe0c84e53284a26bfaf996b002b84d4
Dmitriy Solopov <div.solopov@gmail.com>
2021-08-01 16:50:36 +0300

    first commit

---
+++ ClientApp/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace ClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Query = new System.Windows.Forms.TextBox();
+            this.button_Execute = new System.Windows.Forms.Button();
+            this.dataGridView_Results = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel_Info = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button_DeleteRow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Results)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_Query
+            // 
+            this.textBox_Query.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Query.Location = new System.Drawing.Point(16, 16);
+            this.textBox_Query.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_Query.Name = "textBox_Query";
+            this.textBox_Query.Size = new System.Drawing.Size(816, 36);
+            this.textBox_Query.TabIndex = 0;
+            // 
+            // button_Execute
+            // 
+            this.button_Execute.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Execute.Location = new System.Drawing.Point(843, 10);
+            this.button_Execute.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_Execute.Name = "button_Execute";
+            this.button_Execute.Size = new System.Drawing.Size(217, 50);
+            this.button_Execute.TabIndex = 1;
+            this.button_Execute.Text = "Execute";
+            this.button_Execute.UseVisualStyleBackColor = true;
+            this.button_Execute.Click += new System.EventHandler(this.button_Execute_Click);
+            // 
+            // dataGridView_Results
+            // 
+            this.dataGridView_Results.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView_Results.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Results.Location = new System.Drawing.Point(17, 62);
+            this.dataGridView_Results.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView_Results.Name = "dataGridView_Results";
+            this.dataGridView_Results.RowHeadersWidth = 51;
+            this.dataGridView_Results.Size = new System.Drawing.Size(816, 463);
+            this.dataGridView_Results.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_Info});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 532);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1067, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel_Info
+            // 
+            this.toolStripStatusLabel_Info.ForeColor = System.Drawing.Color.Red;
+            this.toolStripStatusLabel_Info.Name = "toolStripStatusLabel_Info";
+            this.toolStripStatusLabel_Info.Size = new System.Drawing.Size(0, 16);
+            // 
+            // button_DeleteRow
+            // 
+            this.button_DeleteRow.Location = new System.Drawing.Point(843, 110);
+            this.button_DeleteRow.Name = "button_DeleteRow";
+            this.button_DeleteRow.Size = new System.Drawing.Size(217, 62);
+            this.button_DeleteRow.TabIndex = 4;
+            this.button_DeleteRow.Text = "Delete Row";
+            this.button_DeleteRow.UseVisualStyleBackColor = true;
+            this.button_DeleteRow.Click += new System.EventHandler(this.button_DeleteRow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button_DeleteRow);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView_Results);
+            this.Controls.Add(this.button_Execute);
+            this.Controls.Add(this.textBox_Query);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Results)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Query;
+        private System.Windows.Forms.Button button_Execute;
+        private System.Windows.Forms.DataGridView dataGridView_Results;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_Info;
+        private System.Windows.Forms.Button button_DeleteRow;
+    }
+}
+
---
+++ ClientApp/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientApp
+{
+    public partial class Form1 : Form
+    {
+        private string _dp;
+        private string _connectionString;
+
+        private DbProviderFactory _dbProviderFactory;
+        private DbConnection _dbConn;
+        private DataTable _dataTable;
+        public Form1()
+        {
+            InitializeComponent();
+            //toolStripStatusLabel_Info.ForeColor = Color.Red;
+            textBox_Query.Text = "select * from users";
+            _dp = ConfigurationManager.AppSettings["MSSQLProvider"];
+            _connectionString = ConfigurationManager.ConnectionStrings["MSSQLProvider"].ConnectionString;
+            _dbProviderFactory = DbProviderFactories.GetFactory(_dp);
+            _dbConn = _dbProviderFactory.CreateConnection();
+            _dbConn.ConnectionString = _connectionString;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //_dataTable = new DataTable(); //пустая таблица
+            ///*Определяю столбцы*/
+            //_dataTable.Columns.Add("Id");
+            //_dataTable.Columns.Add("Login");
+            //_dataTable.Columns.Add("Email");
+            //_dataTable.Columns.Add("Password");
+
+            ///*создаю строку данных*/
+            //DataRow dataRow = _dataTable.NewRow();
+            //dataRow[0] = 1;
+            //dataRow[1] = "Test user";
+            //dataRow[2] = "testemail.om";
+            //dataRow[3] = "qwerty";
+
+            //_dataTable.Rows.Add(dataRow);
+            ///*привязываю DataTable к DataGridView*/
+            //dataGridView_Results.DataSource = _dataTable;
+        }
+
+
+        private void Execute()
+        {
+            DbDataReader resultReader = null;
+            try
+            {
+                if (textBox_Query.Text.Length >= 5)
+                {
+                    _dataTable = new DataTable();
+                    DbCommand dbSelectAllUsersCommand = _dbProviderFactory.CreateCommand();
+                    dbSelectAllUsersCommand.Connection = _dbConn;
+                    dbSelectAllUsersCommand.CommandText = textBox_Query.Text;
+                    _dbConn.Open();
+                    resultReader = dbSelectAllUsersCommand.ExecuteReader();
+                    // write column names  caps                   
+                    for (int i = 0; i < resultReader.FieldCount; i++)
+                    {
+                        _dataTable.Columns.Add(resultReader.GetName(i));
+                    }
+                    #region resultReader
+                    //resultReader.Read();
+                    //resultReader.Read();
+                    //MessageBox.Show(resultReader[0].ToString());
+                    // MessageBox.Show(resultReader.GetName(0));
+                    // MessageBox.Show(resultReader.FieldCount.ToString());
+                    #endregion
+                    // create _dataTable
+                    while (resultReader.Read())
+                    {
+                        DataRow currentRow = _dataTable.NewRow();
+                        for (int i = 0; i < resultReader.FieldCount; i++)
+                        {
+                            currentRow[i] = resultReader[i];
+                        }
+                        _dataTable.Rows.Add(currentRow);
+                    }
+                    dataGridView_Results.DataSource = _dataTable;
+                }
+                else
+                {
+                    toolStripStatusLabel_Info.Text = "Тело  запроса не может быть пустым или содержать меньше 5 - ти символов ";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                _dbConn.Close();
+                if (resultReader != null) resultReader.Close();
+            }
+        }
+
+
+
+
+        private void button_Execute_Click(object sender, EventArgs e)
+        {
+            Execute();
+        }
+
+
+       
+        private void button_DeleteRow_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow selectedRow in dataGridView_Results.SelectedRows)// dataGridView_Results.SelectedRows - выделенные строки
+            {
+                try
+                {
+                    DbCommand deleteDbCommand = _dbProviderFactory.CreateCommand();// deleteDbCommand - объект для хранения и выполнения SQL запроса
+                    deleteDbCommand.Connection = _dbConn;// соединение
+                                   
+                    int id = Int32.Parse(selectedRow.Cells[0].Value.ToString());// получаем значение  первого столбца выделенной строки 
+                    DbParameter idParam = deleteDbCommand.CreateParameter();
+                    idParam.DbType = System.Data.DbType.Int32;
+                    idParam.ParameterName = "@id";
+                    idParam.Value = id;
+                    deleteDbCommand.Parameters.Add(idParam);
+                  
+                    deleteDbCommand.CommandText = $"DELETE FROM Users WHERE id = @id";
+                    _dbConn.Open();
+                    deleteDbCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    _dbConn.Close();
+                }
+               
+                #region find id
+                // 1 version
+                //DataRowView selected = selectedRow.DataBoundItem as DataRowView;
+                //int id = Int32.Parse(selected.Row[0].ToString());
+
+                // 2 version
+                //int columnIndex = 0;
+                //int id = Int32.Parse(selectedRow.Cells[columnIndex].Value.ToString());
+                //string columnName = dataGridView_Results.Columns[columnIndex].DataPropertyName;
+                //MessageBox.Show($"DELETE FROM Users WHERE {columnName} = {id}");
+
+                // 3 version
+                //int columnIndex = 0;
+                //string value = selectedRow.Cells[columnIndex].Value.ToString();
+                //string columnName = dataGridView_Results.Columns[columnIndex].DataPropertyName;
+                //MessageBox.Show($"DELETE FROM Users WHERE {columnName} = {value}");
+                #endregion
+            }
+            Execute();
+        }
+
+
+
+
+        //private void button_Execute_Click(object sender, EventArgs e)
+        //{
+        //    DbDataReader resultReader = null;
+        //    try
+        //    {
+        //        if (textBox_Query.Text.Length >= 5)
+        //        {
+        //            _dataTable = new DataTable();
+
+        //            DbCommand dbSelectAllUsersCommand = _dbProviderFactory.CreateCommand();
+        //            dbSelectAllUsersCommand.Connection = _dbConn;
+        //            dbSelectAllUsersCommand.CommandText = textBox_Query.Text;
+        //            _dbConn.Open();
+        //            resultReader = dbSelectAllUsersCommand.ExecuteReader();
+        //            //toolStripStatusLabel_Info.Text = resultReader.
+        //            int line = 0;
+        //            do
+        //            {
+        //                while (resultReader.Read())
+        //                {
+        //                    if (line == 0)
+        //                    {
+        //                        for (int i = 0; i < resultReader.FieldCount; i++)
+        //                        {
+        //                            _dataTable.Columns.Add(resultReader.GetName(i));
+        //                        }
+        //                        line++;
+        //                    }
+        //                    DataRow currentRow = _dataTable.NewRow();
+        //                    for (int i = 0; i < resultReader.FieldCount; i++)
+        //                    {
+        //                        currentRow[i] = resultReader[i];
+        //                    }
+        //                    _dataTable.Rows.Add(currentRow);
+        //                }
+        //            } while (resultReader.NextResult());
+        //            dataGridView_Results.DataSource = _dataTable;
+        //        }
+        //        else
+        //        {
+        //            toolStripStatusLabel_Info.ForeColor = Color.Red;
+        //            toolStripStatusLabel_Info.Text = "Тело  запроса не межет быть пустым";
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //    finally
+        //    {
+        //        _dbConn.Close();
+        //        if(resultReader != null) resultReader.Close();
+        //    }
+        //}
+
+
+
+
+
+
+    }
+}
